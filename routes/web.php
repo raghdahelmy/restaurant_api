fc086bbc8c9fac467380dda94ba9a1b866c67f96
Raghda Helmy <raghda.helmy82@gmail.com>
2025-07-13 06:07:32 +0300

    remove Railway

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::get('/', function () {
-    return 'Laravel app is working on Railway!';
+    return view('welcome');
 });
