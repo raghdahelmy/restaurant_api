179d34cb41af23b60faac0c4c67d9addcef174ab
Raghda Helmy <raghda.helmy82@gmail.com>
2025-07-13 05:41:56 +0300

    Add test root route for Railway

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\MealController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,5 @@ Route::controller(CategoryController::class)->group(function(){
 Route::get('/categories', 'index');         // للحصول على كل الأقسام
     Route::get('/categories/{id}/meals', 'meals'); // للحصول على الوجبات بقسم معين
 });
+
+Route::get('/meals',[MealController::class,'index']);
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return 'Laravel app is working on Railway!';
 });
