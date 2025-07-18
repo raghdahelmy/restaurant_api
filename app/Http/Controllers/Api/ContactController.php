cceb811447bb11fa775e9104d795c34d8085dad6
Raghda Helmy <raghda.helmy82@gmail.com>
2025-07-18 22:57:46 +0300

    use contact ApiResponse & ApiResource

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ContactResource;
+use App\Traits\ApiResponse;
 
 class ContactController extends Controller
 {
     //
+
+    use ApiResponse;
     public function store(Request $request)
     {
         $request->validate([
@@ -20,10 +24,12 @@ class ContactController extends Controller
 
    $contact= Contact::create($request->only('name','email','phone','message'));
 
-return response()->json([
-       'status' => true,
-            'message' => 'تم إرسال الرسالة بنجاح',
-            'data' => $contact,
-], 201);
+return $this->SuccessResponse(new ContactResource($contact),'تم إرسال الرسالة بنجاح',201);
+
+// return response()->json([
+//        'status' => true,
+//             'message' => 'تم إرسال الرسالة بنجاح',
+//             'data' => $contact,
+// ], 201);
     }
 };
